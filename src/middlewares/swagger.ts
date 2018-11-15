65f91f18084badc7c7df14399886d9617c220b06
Mikel Sanchez <misanche@redhat.com>
2018-11-15 15:29:10 +0100

    Updated to OpenAPI 3

--- src/middlewares/swagger.ts
+++ src/middlewares/swagger.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
-import swaggerTools = require("swagger-tools");
-import { Swagger20Request } from "swagger-tools";
+import swaggerTools = require("oas-tools");
+import { Swagger20Request } from "oas-tools";
 import { readFileSync } from "fs";
 import * as YAML from "js-yaml";
 declare module "express" {
@@ -17,26 +17,14 @@ export const initSwaggerMiddlware = function (app: express.Express, basePath: st
     const swaggerDoc = loadDocumentSync(basePath + "/definition/swagger.yaml");
     const options = {
         controllers: basePath + "/routes",
-        ignoreMissingHandlers: true,
-        useStubs: false,
-        validateResponse: true
+        loglevel: "info",
+        strict: true,
+        router: true,
+        validator: true,
+        docs: !isProd
     };
-    swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-        // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-        app.use(middleware.swaggerMetadata());
-
-        // Validate Swagger requests
-        app.use(middleware.swaggerValidator({
-
-        }));
-
-        // Route validated requests to appropriate controller
-        app.use(middleware.swaggerRouter(options));
-        if (!isProd) {
-            // Serve the Swagger documents and Swagger UI
-            app.use(middleware.swaggerUi());
-        }
+    swaggerTools.configure(options);
+    swaggerTools.initialize(swaggerDoc, app, function() {
         cb();
-
     });
 };
